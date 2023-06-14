66f80d0201c8b6b02555d74fecac59e4d768f99c
ManishaSalunke <msalunke928@gmail.com>
2023-06-14 16:06:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const bill = document.querySelector('#BA');
 const discount = document.querySelector('#DP');
 const calculator = document.querySelector('#cal')
 const total = document.querySelector('#total');
+
+
+
 calculator.addEventListener('click', () => {
     const billValue = bill.value;
     const discountValue = discount.value;
@@ -17,27 +20,6 @@ calculator.addEventListener('click', () => {
 
 function isValid(discount) {
     if (discount < 0 || discount > 100) {
-        return false;
-    }
-    return true;
-}
-
-
-calculator.addEventListener('click', () => {
-    const billValue = bill.value;
-    const discountValue = discount.value;
-    const validBill = isValidBill(billValue);
-    if (validBill) {
-        const discountAmount = billValue - (billValue * discountValue) / 100;
-        total.innerHTML = `Total amount to pay is:${discountAmount}`;
-    } else {
-        alert(`Entered bill is incorrect${billValue}`);
-    }
-
-});
-
-function isValidBill(bill) {
-    if (bill < 0 || bill == "") {
         return false;
     }
     return true;
